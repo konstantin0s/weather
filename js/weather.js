3d8aabfd86cba8975b45baf11c7883fca3d60c9f
konstantin0s <constantintofan85@gmail>
2018-02-20 22:03:11 +0100

    Add js

---
+++ js/weather.js
@@ -0,0 +1,76 @@
+// if("geolocation" in navigator) {
+//   navigator.geolocation.getCurrentPosition(function(position) {
+//     loadWeather(position.coords.latitude + ',' + position.coords.longitude);
+//   });
+// } else {
+//   loadWeather("Amsterdan, NL", "");
+// }
+//
+// $(document).ready(function() {
+//   setInterval(getWeather, 10000);
+// });
+//
+// function loadWeather(location, woeid) {
+//   $.simpleWeather({
+//     location: location,
+//     woeid: woeid,
+//     unit: 'c',
+//     success: function(weather) {
+//       city = weather.city;
+//       temp = weather.temp;
+//       wcode = '<img class="weathericon" src="images/weathericons' + weather.code + '.svg">';
+//
+//       wind = '<p>' + weather.wind.speed + '</p><p>' + weather.units.speed + '</p>';
+//       humidity = weather.humidity + ' %';
+//
+//       $(".location").text(city);
+//       $(".temperature").html(temp);
+//       $(".climate_bg").html(wcode);
+//       $(".windspeed").html(wind);
+//       $(".humidity").text(humidity);
+//     },
+//
+//     error: function(error) {
+//       $(".error").html('<p>' + error + '</p>');
+//     }
+//   });
+// }
+
+
+/* Does your browser support geolocation? */
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    loadWeather(position.coords.latitude+','+position.coords.longitude); //load weather using your lat/lng coordinates
+  });
+} else {
+  $('.not-available').show();
+  loadWeather('Amsterdam, NL',''); //fallback
+}
+
+$(document).ready(function() {
+  setInterval(getWeather, 30000);
+});
+
+function loadWeather(location, woeid) {
+  $.simpleWeather({
+    location: location,
+    woeid: woeid,
+    unit: 'c',
+    success: function(weather) {
+      city = weather.city;
+      temp = weather.temp+'&deg;';
+      wcode = '<img class="weathericon" src="images/weathericons/' + weather.code + '.svg">'
+      wind = '<p>'+weather.wind.speed+'</p><p>'+weather.units.speed+'</p>'
+      humidity = weather.humidity + ' %';
+
+      $(".location").text(city)
+      $(".temperature").html(temp);
+      $(".climate_bg").html(wcode);
+      $(".windspeed").html(wind);
+      $(".humidity").text(humidity);
+    },
+    error: function(error) {
+      $(".city-weather").html('<p>'+error+'</p>');
+    }
+  });
+}
